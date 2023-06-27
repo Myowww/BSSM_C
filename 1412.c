072a1200aa4fb01a3315728d5a0f425c0c50ac59
myowww <eahenlee@gmail.com>
2023-06-27 15:13:01 +0900

    1412 오류 수정

--- 1412.c
+++ 1412.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-int main(){
-    char sen[90]={};
-    gets(sen);  
+
+int main() {
+    char sen[90] = {};
+
+    fgets(sen, sizeof(sen), stdin);
     // 배열에서 공백이나 특수문자 포함 문자열 입력
-    
-    for(int i=97; i<=122; i++){ 
-    // 소문자 ASCII CODE a~z : 97~122
-    
-        int num=0;  // 몇 번 나왔는 지 셀 변수 생성성
-    
-        for(int j=0; sen[j]!='\n'; j++){    
-            // j번째 문자가 \n이 아닐 때까지 반복
-            
-            if(sen[j]==i){  
+    // 버퍼 오버플로우를 방지하기 위해 fgets 사용
+
+    for (int i = 'a'; i <= 'z'; i++) {
+        int num = 0; // 문자 발생 횟수를 세는 변수
+
+        for (int j = 0; sen[j] != '\0'; j++) {
+            if (sen[j] == i) {
             // 만약 sen 배열의 j번째 문자가 i(알파벳의 ASCII CODE)와 같다면
-                
-                num++; 
-                // 횟수 증가
-                
+                num++; // 발생 횟수 증가
             }
-            
-        printf("%c:%d\n",i,num);
+        }
+
+        if (num >= 0) {
+            printf("%c:%d\n", i, num);
         // '알파벳 : 횟수' 출력
+        }
     }
-    
+
     return 0;
 }
