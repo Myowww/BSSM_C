575811aff4aba35b0dd55de97d36e9ae00dc2ee9
YeHen Lee <126836473+Myowww@users.noreply.github.com>
2023-07-26 11:31:21 +0900

    1412

--- 1412.c
+++ 1412.c
@@ -4,8 +4,8 @@ int main() {
     char sen[90] = {};
 
     fgets(sen, sizeof(sen), stdin);
-    // 배열에서 공백이나 특수문자 포함 문자열 입력
-    // 버퍼 오버플로우를 방지하기 위해 fgets 사용
+    // 배열에서 공백이나 특수문자를 포함한 문자열 입력
+    // 버퍼 오버플로우를 방지하기 위해 fgets 사용 (fgets는 공백이나 특수문자까지 다 받는 scanf라고 생각하면 됨)
 
     for (int i = 'a'; i <= 'z'; i++) {
         int num = 0; // 문자 발생 횟수를 세는 변수
