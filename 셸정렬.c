bea6bd5eb366ea96260462c201f175e203a12e23
YeHen Lee <126836473+Myowww@users.noreply.github.com>
2023-09-25 10:17:27 +0900

    셸정렬

---
+++ 셸정렬.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void insertsort(int list[], int first, int last, int gap){
+    
+    int i, j, key;
+    
+    for(i=first+gap; i<=last; i+=gap);{
+        
+        key = list[i];
+        
+        for(j=i-gap; j>=first && key<list[j]; j-=gap)
+            list[j+gap] = list[j];
+        list[j+gap] = key;
+        
+    }
+    
+}
+
+
+
+int main()
+{
+    
+    int list[] = {10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16};
+    int i, j, gap, n;
+    
+    n = sizeof(list) / sizeof(int);
+    
+    for(gap=n/2; gap>0; gap/=2){
+        
+        if((gap%2)==0) gap++;
+        
+        for(i=0; i<gap; i++){
+            insertsort(list, i, n-1, gap);
+        }
+        
+    }
+    
+    for(i=0; i<n; i++)
+        printf("%d ", list[i]);
+    
+    return 0;
+}
+
+
